24177a2fe8b2844b1527041c6f19d82599e583a1
rafaexperience <erreemece@live.com>
2015-07-03 09:52:50 +0200

    incluyo pdf's y quito .sln de git

---
+++ PDFs_Modulo2/Mod1 - JS  serv. y Node JS (variables, tipos y funciones)/Miriadax_Modulo1_Ej_Opcional.js
@@ -0,0 +1,7 @@
+﻿//Realizar en JavaScript para node.js un programa que saque por consola
+
+//1) El número PI con ocho decimales
+//2) Dos línea en blanco
+//3)  La ristra de caracteres UNICODE
+//       -> \u55e8\uff0c\u4f60\u597d\u5417
+
